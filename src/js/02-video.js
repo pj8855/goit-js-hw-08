49d164b95193dcfd8e0bb29e8019e35b6649f89e
pj8855 <115658268+pj8855@users.noreply.github.com>
2023-05-31 22:14:25 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Vimeo from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
 
+player.on('play', function() {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+  console.log('title:', title);
+});
